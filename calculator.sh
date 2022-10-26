52032e9a9cde602c5290041df0753249565ad7fa
jaboor <jaboordandan14@gmail.com>
2022-10-26 15:37:09 +0300

    First commit

---
+++ calculator.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+#add()----------------------------------------------------#
+#input : two integers
+#integers output : on integer as answer
+#---------------------------------------------------------#
+
+add(){
+    local answer=0
+    for sum in $@
+    do
+        let answer=$answer+$sum
+    done
+        echo $answer
+}
+
+#display()------------------------------------------------#
+#this function get the answer and display it and another three options
+#input  : 1 number as answer
+#output : print on the display 
+#used_function :is_prime() & is_odd_even() & is_div_by_five()
+#---------------------------------------------------------#
+
+display(){
+    local answer=$1
+    echo
+    echo the answer is $answer
+    is_prime $answer
+    is_odd_even $answer
+    is_div_by_five $answer
+    echo
+}
+
+#calculate()----------------------------------------------#
+#function that get two numbers and one operatoras number then use other functions to calculate
+#input  : multiple integers, first argument operator , after  inputs numbers 
+#output : 1 number as answer
+#used_function : add() & subtract() & multiply() & divide() & power_of() modulus_%()
+#---------------------------------------------------------#
+
+calculate() {
+    local operator=$1
+    local numbers=$(echo $@ | cut -d" " -f 2-)
+    case $operator in
+        1)
+            add $numbers
+        ;;
+        
+    esac
+}
+#input()--------------------------------------------------#
+#funcation that ask the user for two integers (can be positive or negative) and validate the input
+#validation : not zero or one input , the inputs are positive or negative integers 
+#input      : multiple  numbers , one argument and several integers
+#outbut     : excute display or ask the user to insert again
+#used_functions : display
+#---------------------------------------------------------#
+
+input () {
+    local re='^[+-]?[0-9]+$'
+    local operator=$1
+    local tester=1
+    while [[ $tester -eq 1 ]]
+    do
+        tester=2
+	    echo -n "please enter 2 numbers " 
+	    read -a arr_num
+	    if [ ${#arr_num[@]} -lt 2 ]
+        then
+	        tester=1
+            echo "please enter at least 2 arguments "
+	    echo
+	    else # test the array members
+		for num in ${arr_num[@]}
+		do
+		if  [[ ! $num =~ $re ]] 
+		then
+		    tester=1
+		fi
+		done
+	    [ $tester -eq 1 ] && echo -e "please enter integer only\n"
+	    fi
+    done
+    display $(calculate $operator ${arr_num[@]})
+}
+
+#menu()---------------------------------------------------#
+#funcation that display menu for the user to select an operation
+#input  : None
+#output : display()
+#usedfunctions: input() 
+#---------------------------------------------------------#
+
+function menu() {
+    select menu_answer in Add Subtract Multiply Divide Power_of Modulos Exit
+    do
+        local action=0;
+    case $menu_answer in
+        Add)
+            action=1
+            echo "$menu_answer selected"
+        ;;
+        Subtract)
+            action=2
+            echo "$menu_answer selected"
+        ;;
+        Multiply)
+            action=3
+            echo "$menu_answer selected"
+        ;;
+        Divide)
+            action=4
+            echo "$menu_answer selected"
+        ;;
+        Power_of)
+            action=5
+            echo  "$menu_answer selected"
+        ;;
+        Modulos)
+            action=6
+            echo  "$menu_answer selected"
+        ;;
+        Exit)
+            exit 1
+        ;;
+        *)
+            echo -e "Try again, your input should be between 1-7\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n5) Power_of\n6) Modulos\n7) Exit "
+        ;;
+    esac
+        [ $action -ne 0 ] 	&& input $action 	 #needed  to skip if case is *
+
+    done
+}
+
+menu
+
+
